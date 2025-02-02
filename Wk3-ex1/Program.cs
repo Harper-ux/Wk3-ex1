dca1c7abadf54063d36ceb61da76148f44e397de
harper Shafer <soareaper63@gmail.com>
2025-02-02 11:08:33 -0600

    comments have been added to the cases and the if scenario

--- Wk3-ex1/Program.cs
+++ Wk3-ex1/Program.cs
@@ -23,33 +23,33 @@ namespace Wk3_ex1
                 double grade = Convert.ToDouble(Console.ReadLine());
                 // Use switch statement to determine the letter grade
 
-                if (grade > 0 || grade < 100)
+                if (grade > 0 || grade < 100) // Check if the grade is within the range of 0-100
                 { 
                 }
 
-                grade = grade / 10;
+                grade = grade / 10; // Divide the grade by 10 to get the first digit of the grade
                 Console.WriteLine( "Your letter grade is:  " );
-                switch (grade / 10)
+                switch (grade / 10) // Use the first digit of the grade to determine the letter grade
                 {
-                    case 10:
-                    case 9:
-                        grade = 'A';
+                    case 10:        // If the grade is 100
+                    case 9:        // If the grade is between 90-99
+                        grade = 'A';    // Assign the letter grade A
                         break;
-                    case 8:
-                        grade = 'B';
+                    case 8:       // If the grade is between 80-89
+                        grade = 'B';    // Assign the letter grade B
                         break;
-                    case 7:
-                        grade = 'C';
+                    case 7:      // If the grade is between 70-79
+                        grade = 'C';    // Assign the letter grade C
                         break;
-                    case 6:
-                        grade = 'D';
+                    case 6:     // If the grade is between 60-69        
+                        grade = 'D';      // Assign the letter grade D
                         break;
-                    case 5: 
-                        grade = 'F';
+                    case 5:      // If the grade is between 50-59
+                        grade = 'F';   // Assign the letter grade F
                         break;
                 }
 
-                // Output the letter grade
+                // Output the letter grade 
                 Console.WriteLine($"Your letter grade is:  " + grade);
             }
         }
